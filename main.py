f6d1ac3882e0b2b41d1aaeefb67624ef3bf7299f
charlie <charliecondorcet@gmail.com>
2021-01-16 22:24:08 -0300

    feat(main/fibonacciCalc): add the fibonacci calculator

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+
+def fibonacciCalc(num):
+    """ Calculate for find the Fibonacci code with the number asigned """
+    if num < 2:
+        return num
+
+    else:
+        # calculate the fibonacci code
+        i = fibonacciCalc(num-1)
+        j = fibonacciCalc(num-2)
+
+        return i + j
+
+
 if __name__ == "__main__":
     print('hola mundo')
