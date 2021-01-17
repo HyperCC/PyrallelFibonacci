dbaf0aa15de0ee09881c037afad65f8f2c55d2fa
charlie <charliecondorcet@gmail.com>
2021-01-16 22:26:11 -0300

    feat(main/factorization): add the factorization function

--- main.py
+++ main.py
@@ -12,5 +12,29 @@ def fibonacciCalc(num):
         return i + j
 
 
+def factorization(fib):
+    """ Numeric factorization for a fibonacci number """
+
+    divisor = 2
+    values = []
+
+    # save unfactorizable values
+    if fib <= 5:
+        values.append(str(fib))
+
+    else:
+        while fib != 1:
+
+            # finding divisors
+            if fib % divisor == 0:
+                values.append(str(divisor))
+                fib = fib/divisor
+
+            else:
+                divisor += 1
+
+    return values
+
+
 if __name__ == "__main__":
     print('hola mundo')
